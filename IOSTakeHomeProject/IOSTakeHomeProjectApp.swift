8cca142cbcae983313d80f276dc4cff5a8f7dec0
sYegin1004 <160428572+sYegin1004@users.noreply.github.com>
2024-08-26 08:14:44 +0300

    Initial Commit

---
+++ IOSTakeHomeProject/IOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTakeHomeProjectApp.swift
+//  IOSTakeHomeProject
+//
+//  Created by serhat on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
